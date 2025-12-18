9cee4d73223b729ebdbf908c23db4a50674095e6
fantom <fantom@DESKTOP-K4T4BM2>
2025-12-18 15:22:16 +0200

    test commit static int a

--- Equipment/Form1.cs
+++ Equipment/Form1.cs
@@ -2,9 +2,12 @@ namespace Equipment
 {
     public partial class Form1 : Form
     {
+        static int a = 4;
         public Form1()
         {
             InitializeComponent();
         }
+
+
     }
 }
